1989394765b8c261653bc7b132693fd3912ff2cb
trankhahao7 <trankhahao7@gmail.com>
2025-05-14 10:07:21 +0700

    test

---
+++ GroupMeeting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOADProject
+{
+    public class GroupMeeting
+    {
+        [Key]
+        public string ID_Group { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<User> Participants { get; set; } = new List<User>();
+    }
+}
---
+++ Model1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace OOADProject
+{
+    public class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Appoinment> Appointments { get; set; }
+        public virtual DbSet<Reminder> Reminders { get; set; }
+        public virtual DbSet<GroupMeeting> GroupMeetings { get; set; }
+        public virtual DbSet<Reminder> Reminders { get; set; }
+
+    }
+}
